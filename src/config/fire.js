1a8a6c54b4712aab4f3fb3e29422d4b5aa8b03ba
lmavaiya <lmavaiya@gmail.com>
2019-06-12 19:21:01 +0530

    first commit

--- src/config/fire.js
+++ src/config/fire.js
@@ -2,24 +2,14 @@
 import app from 'firebase/app';
 import auth from 'firebase/auth';
 
-var config = {
-    apiKey: "AIzaSyA2AUSpY-OwfIiW2U6iVwJ-YxhYiwibRhU",
-    authDomain: "letsreverseit.firebaseapp.com",
-    databaseURL: "https://letsreverseit.firebaseio.com",
-    projectId: "letsreverseit",
-    storageBucket: "letsreverseit.appspot.com",
-    messagingSenderId: "295061171612",
-    appId: "1:295061171612:web:40ba3514e0be455f"
-  };
-
   var config = {
-    apiKey: "AIzaSyA2AUSpY-OwfIiW2U6iVwJ-YxhYiwibRhU",
-    authDomain: "letsreverseit.firebaseapp.com",
-    databaseURL: "https://letsreverseit.firebaseio.com",
-    projectId: "letsreverseit",
-    storageBucket: "letsreverseit.appspot.com",
-    messagingSenderId: "295061171612",
-    appId: "1:295061171612:web:40ba3514e0be455f"
+    apiKey: "xxxxxxxxxxxxx-xxxxxxxxx-xxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
   };
 // Initialize Firebase
 const fire = app.initializeApp(config);
